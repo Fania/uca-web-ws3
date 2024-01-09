09313bf8081e7b6cdf47b1fffc8a9c44431c7be8
Fania Everitt <me@fania.eu>
2024-01-09 22:39:10 +0000

    working spaghetti

--- ex3s/js/scripts.js
+++ ex3s/js/scripts.js
@@ -6,44 +6,51 @@ const [...stars] = document.querySelectorAll('[id^="star"]');
 // add eventListener to all stars
 stars.map(star => 
   star.addEventListener('click', (ev) => {
-    console.log(`clicked ${ev.srcElement.parentNode.id}`);
     captureClick(star);
   })
 );
 
 // pass element on to set class and localStorage
 function captureClick(elem) {
-  changeClass(elem);
+  toggleClass(elem);
   setState(elem);
 }
 
-// change class for element
-function changeClass(elem) {
-  ![...elem.classList].includes("liked") ? 
-    elem.classList.add("liked"):
+function setClass (elem, state) {
+  if (state === "true") {
+    elem.classList.add("liked");
+  }
+  if (state === "false") {
     elem.classList.remove("liked");
+  }
 }
 
+function toggleClass(elem) {
+  elem.classList.toggle("liked");
+}
 // update localStorage for element
-function setState(elem) {
+function setState(elem) { 
   [...elem.classList].includes("liked") ? 
     localStorage.setItem(elem.id, true):
     localStorage.setItem(elem.id, false);
 }
 
-loadFromStorage();
+function getState(elem) {
+  let stateStored = localStorage.getItem(elem.id);
+  if (stateStored !== null && stateStored) {
+    console.log(`getting state bool ${elem.id}`, stateStored);
+    setClass(elem, stateStored);
+    return stateStored;
+  }
+}
+
 function loadFromStorage() {
-  console.log('loading from storage');
+  let starState;
+  
   stars.map(star => {
-
-    // when loading the localstorage applies everything right, the classes are right, the localstorage is right, etc, but then when loading the page for some reason its loaded wrong!
-    
-    console.log('star.id == true',Boolean(localStorage.getItem(star.id)) === true);
-
-    if(Boolean(localStorage.getItem(star.id)) === true){
-      star.classList.add("liked");
-    } else {
-      star.classList.remove("liked");
-    }
+    starState = getState(star);
+    setClass(star, starState);
   })
 }
+
+document.addEventListener('DOMContentLoaded', loadFromStorage);
